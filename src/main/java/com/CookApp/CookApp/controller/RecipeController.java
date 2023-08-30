e27a75e0134e73fe177ff374366392433d513003
MaksymilianSt <Maksymilianst02@gmail.com>
2023-08-30 20:32:44 +0200

    Recipe refactor

--- src/main/java/com/CookApp/CookApp/controller/RecipeController.java
+++ src/main/java/com/CookApp/CookApp/controller/RecipeController.java
@@ -47,54 +47,5 @@ public class RecipeController {
             return ResponseEntity.badRequest().build();
         }
     }
-//
-//    @GetMapping("/test")
-//    public void test() {
-//        logger.info("Jestem w metodzie :    JD kurwe");
-//        Recipe recipe = new Recipe();
-//        recipe.setName("pomidorowa");
-//        recipe.setDescription("po rosole z wczoraj");
-//        recipe.setCategory(RecipeCategory.SOUP);
-//        recipe.setDifficultyLevel(DifficultyLevel.EASY);
-//        Ingredient ingredient = new Ingredient();
-//        ingredient.setName("pomidor");
-//        ingredient.setCarbQuantityPer100Gram(10.0);
-//        ingredient.setDescription("malinowy ozarowski");
-//
-//        Recipe savedRec = repository.save(recipe);
-//        Ingredient savedIngr = ingredientRepository.save(ingredient);
-//
-//        RecipeIngredient recipeIngredient = new RecipeIngredient(ingredient, recipe, 22);
-////        recipeIngredient.setId(new RecipeIngredientKey(savedRec.getId(),savedIngr.getId()));
-////        recipeIngredient.setRecipe(recipe);
-////        recipeIngredient.setIngredient(ingredient);
-////        recipeIngredient.setQuantity(69);
-//        Recipe recipe2 = new Recipe();
-//        recipe2.setName("pgryzbowa");
-//        recipe2.setDescription("po pomidorowe z wczoraj");
-//        recipe2.setCategory(RecipeCategory.DESSERT);
-//        recipe2.setDifficultyLevel(DifficultyLevel.HARD);
-//        //recipe2.addIngredient(ingredient);
-//        repository.save(recipe2);
-//
-//        recipeIngredientRepository.save(new RecipeIngredient(ingredient, recipe2, 55));
-//        recipeIngredientRepository.save(recipeIngredient);
 
-//          }
-
-    @GetMapping("/test2")
-    @Transactional
-    public void test2() {
-//        Recipe recipe = new Recipe();
-//        recipe.setName("test2");
-//        recipe.setDescription("test 2 desc");
-//        logger.info("\t\ttest2");
-//        Ingredient ingredient = ingredientRepository.findAll().stream().filter(e-> e.getId() == 13).findAny().get();
-//
-//        logger.info(ingredient != null?"nie null":"null");
-//        logger.info("rozmiar przepisow : " +ingredient.getRecipes().size());
-//        ingredient.getRecipes().forEach( e-> e.getRecipe().setName("hihi"));
-
-
-    }
 }
